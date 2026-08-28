7571e6e9b8afc4f62fc4f8ebc2bf4c36cff6652f
Your Name <you@example.com>
2026-08-27 20:47:57 -0500

    creacion de la clase principal

--- lab1/src/main/java/org/example/lab1/Lab1Application.java
+++ lab1/src/main/java/org/example/lab1/Lab1Application.java
@@ -1,13 +1,11 @@
 package org.example.lab1;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// esta es la clase principal, de aca arranca toda la aplicacion
 @SpringBootApplication
 public class Lab1Application {
-
     public static void main(String[] args) {
+        // esto levanta el servidor embebido y cargara todo el conetxto del spring
         SpringApplication.run(Lab1Application.class, args);
     }
-
 }
