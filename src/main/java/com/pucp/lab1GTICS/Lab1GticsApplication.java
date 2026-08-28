0fdf4066b8997850869dad098d4bf9d15f1e1b29
Your Name <you@example.com>
2026-08-27 20:25:29 -0500

    primer commit en la repo

---
+++ src/main/java/com/pucp/lab1GTICS/Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.pucp.lab1GTICS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1GticsApplication.class, args);
+	}
+
+}
